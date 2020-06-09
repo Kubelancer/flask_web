907edbbb005c1cd7022c55bb685016bbb332a983
Kubelancer <kubelancer@gmail.com>
2020-06-09 06:05:22 +0000

    commit1

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:16.04
+
+RUN apt-get update -y && \
+    apt-get install -y python-pip python-dev
+
+RUN pip install Cython
+RUN pip install ddtrace
+RUN pip install request
+RUN pip install requests
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+
+COPY . /app
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "app.py" ]
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+from ddtrace import tracer
+from flask import request
+import requests
+
+def get_aws_ip():
+  r = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4')
+  return r.text
+
+tracer.configure(hostname=get_aws_ip())
+
+app = Flask(__name__) 
+
+@app.route('/') 
+def hello(): 
+	return "welcome to the flask tutorials"
+
+
+if __name__ == "__main__": 
+	app.run(host ='0.0.0.0', port = 5005, debug = True) 
+
